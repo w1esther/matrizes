de88e8ff60127299c1271d2841b15311317a8145
w1esther <w1mariaesther@gmail.com>
2026-09-11 22:39:21 -0300

    testes para implementar

--- testes/criar_matriz.py
+++ testes/criar_matriz.py
@@ -1,8 +1,10 @@
 from manim import *
 import numpy as np
 
-class Matriz(Scene):
+class Matriz(MovingCameraScene):
     def construct(self):
+
+        self.play(self.camera.frame.animate.scale(1.5))
 
         titulo = Text('Soma de Matrizes', font_size=32).shift(3*UP)
 
@@ -16,17 +18,31 @@ class Matriz(Scene):
             [termo_matriz('a', 1, 1), termo_matriz('a', 1, 2), termo_matriz('a', 1, 3)],
             [termo_matriz('a', 2, 1), termo_matriz('a', 2, 2), termo_matriz('a', 2, 3)],
             [termo_matriz('a', 3, 1), termo_matriz('a', 3, 2), termo_matriz('a', 3, 3)]]).shift(1*UP+5*LEFT)
+
+        a_11 = MathTex("a_{11}").move_to(A_generica.get_entries()[0])
         
         B_generica = MobjectMatrix([
             [termo_matriz('b', 1, 1), termo_matriz('b', 1, 2), termo_matriz('b', 1, 3)],
             [termo_matriz('b', 2, 1), termo_matriz('b', 2, 2), termo_matriz('b', 2, 3)],
             [termo_matriz('b', 3, 1), termo_matriz('b', 3, 2), termo_matriz('b', 3, 3)]], v_buff=0.75).shift(1*UP+0.5*LEFT)
         
+        C_generica = MobjectMatrix([
+            [MathTex(r"c_{11} + b_{11}"), MathTex(r"c_{12} + b_{12}"), MathTex(r"c_{13} + b_{13} + a_{13} + b_{11} + b_{11}")],
+            [MathTex(r"c_{21} + b_{11}"), MathTex(r"c_{22} + b_{11}"), MathTex(r"c_{23} + b_{11}")],
+            [MathTex(r"c_{31} + b_{11}"), MathTex(r"c_{32} + b_{11}"), MathTex(r"c_{33} + b_{11}")]
+        ], v_buff=0.75).shift(1*UP+5*RIGHT)
+
+        for entrada in C_generica.get_entries():
+            entrada.set_opacity(0)
+        
         mais2 = MathTex(r'+').shift(1*UP + 2.75*LEFT)
         
-        self.play(FadeIn(A_generica), FadeIn(B_generica),FadeIn(mais2), run_time = 2)
+        self.play(FadeIn(A_generica), FadeIn(B_generica),FadeIn(mais2),FadeIn(a_11),FadeIn(C_generica), run_time = 2)
+        self.play(a_11.animate.move_to(C_generica.get_entries()[0]).shift(0.5*LEFT))
 
         self.wait(5)
+
+        self.play(FadeOut(A_generica), FadeOut(B_generica), FadeOut(mais2), FadeOut(a_11), FadeOut(C_generica))
 
         A = Matrix([
             [1, 2, 3],
